ac7c189c84d8f57a7262252a45abfbabbd6c44e0
arsalanans47 <arsalanans8377@gmail.com>
2026-09-05 00:18:12 +0530

    Added the backend deployment API in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://emi-store-1hok.onrender.com/',
     },
   },
 })
